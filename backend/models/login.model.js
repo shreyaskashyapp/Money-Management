d595b483a3fd206fbd1637f8beb9702aede5002e
shreyaskashyapp <shreyaskashyap2002@gmail.com>
2022-12-17 00:42:30 +0530

    asd

--- backend/models/login.model.js
+++ backend/models/login.model.js
@@ -3,7 +3,7 @@ const Schema =mongoose.Schema;
 
 const loginSchema=new Schema({
     username:{type:String,require:true},
-    login_id:{type:int,require:true},//why login id??
+    login_id:{type:int,require:true},//why login id?
     password:{type:String,require:true},
 },{
     timestamps:true,
