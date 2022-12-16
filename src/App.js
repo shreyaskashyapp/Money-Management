2a4c5f0ec58c334d6af670ec5d1bb52b6d888e3d
shreyaskashyapp <shreyaskashyap2002@gmail.com>
2022-12-16 23:45:31 +0530

    frontendstuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./components/home"
+import Reports from "./components/reports"
+import Visualise from "./components/visualise"
+import Settings from "./components/settings"
+import Navbar from "./components/navbar"
+import react from 'react'
+import "./App.css"
+
+console.log(Navbar)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="nav-container">
+          <Navbar />
+          <Routes>
+            <Route path='/' exact element={<Home />}/>
+            <Route path='/reports' element={<Reports />}/>
+            <Route path='/visualise' element={<Visualise />}/>
+            <Route path='/settings' element={<Settings />}/>
+          </Routes>
+      </div>
+    </Router>  
   );
 }
 
